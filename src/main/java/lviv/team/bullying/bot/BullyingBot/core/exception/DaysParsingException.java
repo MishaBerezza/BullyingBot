5d18442d88687b8068b03181acf8035b00ff553a
mishaberezhinsky <berezhinskiy.m@gmail.com>
2025-06-11 01:19:10 +0300

    formatt code

--- src/main/java/lviv/team/bullying/bot/BullyingBot/core/exception/DaysParsingException.java
+++ src/main/java/lviv/team/bullying/bot/BullyingBot/core/exception/DaysParsingException.java
@@ -1,2 +1,11 @@
-package lviv.team.bullying.bot.BullyingBot.core.exception;public class DaysParsingException {
+package lviv.team.bullying.bot.BullyingBot.core.exception;
+
+public class DaysParsingException extends BullingBotException {
+    private static final String EXCEPTION_MESSAGE = "Days parsing exception";
+
+
+    public DaysParsingException() {
+        super(EXCEPTION_MESSAGE);
+    }
+
 }
